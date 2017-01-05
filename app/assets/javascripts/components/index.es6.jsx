97f4e0f96b95897d820a604c1a3335daf50c3fbf
codey4k <codey4k@gmail.com>
2017-01-05 16:29:57 +0000

    Add index controller and render first view

---
+++ app/assets/javascripts/components/index.es6.jsx
@@ -0,0 +1,10 @@
+class Index extends React.Component {
+  render () {
+    return ( 
+      <div>
+        <h1>Welcome to my new app rails-react</h1>
+        <p>by Codey4k</p>
+      </div>
+      )
+  }
+}
