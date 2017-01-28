877abb6ebce00a768b34ecec7a47ee3d103cc022
codey4k <codey4k@gmail.com>
2017-01-28 00:32:00 +0000

    upgrade 27/01/2017

--- app/assets/javascripts/components/index.es6.jsx
+++ app/assets/javascripts/components/index.es6.jsx
@@ -1,10 +1,12 @@
+//index view
+
 class Index extends React.Component {
-  render () {
-    return ( 
-      <div className="index">
-        <h1>Welcome to my new app rails-react</h1>
-        <p>by &#64;Codey4k</p>
-      </div>
-    )
-  }
+    render () {
+        return (
+            <div className="index">
+                <h2>Hi</h2>
+            </div>
+        )
+    }
 }
+
--- app/assets/javascripts/components/nav_bar.es6.jsx
+++ app/assets/javascripts/components/nav_bar.es6.jsx
@@ -1,16 +1,9 @@
+//navBar component
+
 class NavBar extends React.Component {
-  render () {
-    return (
-        <div className="navBar">
-          <div className="navBar__container">
-            <h2>Logo</h2>
-            <nav>
-              <a>Home</a>
-              <a>Enterprise</a>
-              <a>About</a>
-            </nav>
-          </div>
-        </div>
-    )
-  }
+    render() {
+        return(
+            <div className="navBar"></div>
+        )
+    }
 }
