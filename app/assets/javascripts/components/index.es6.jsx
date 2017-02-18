e087bb8453f7c24a7cde615309bd15f930825882
codey4k <codey4k@gmail.com>
2017-02-18 00:27:15 +0000

    17/02/2017

--- app/assets/javascripts/components/index.es6.jsx
+++ app/assets/javascripts/components/index.es6.jsx
@@ -4,7 +4,12 @@ class Index extends React.Component {
     render () {
         return (
             <div className="index">
-                <div className="hero"></div>
+                <div className="hero">
+                    <div className="hero__container">
+                        <h1>Hello World!</h1>
+                        <p>This is my app</p>
+                    </div>
+                </div>
             </div>
         )
     }
