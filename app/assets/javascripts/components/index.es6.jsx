4d0b0177282e092890c78f7e44bc739c11486c11
codey4k <codey4k@gmail.com>
2017-04-23 22:47:31 +0000

    23/04/2017

--- app/assets/javascripts/components/index.es6.jsx
+++ app/assets/javascripts/components/index.es6.jsx
@@ -5,7 +5,8 @@ class Index extends React.Component {
         return (
             <div className="index">
                 <div className="hero">
-                   
+                    <img src="<%= asset_url('assets/image.jpg') %>"/>
+                    <h2>Happy hacking!</h2>
                 </div>
                 <Main />
             </div>
