9bf796ad7f1911ecb766622836fe014402f3df1a
codey4k <codey4k@gmail.com>
2017-12-22 10:02:23 +0000

    22/12/2017

--- app/assets/javascripts/components/nav_bar.es6.jsx
+++ app/assets/javascripts/components/nav_bar.es6.jsx
@@ -1,15 +1,56 @@
 //navBar component
+// import React from 'react'
 
 class NavBar extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            alto : 80,
+            background : 'transparent'
+        }
+        this.scrollHandler = this.scrollHandler.bind(this)
+    }
+    
+    componentDidMount(){
+        window.addEventListener('scroll',this.scrollHandler)
+    }
+    
+    componentWillUnmount(){
+        window.removeEventListener('scroll',this.scrollHandler)
+    }
+    
+    scrollHandler() {
+        let scroll = document.documentElement.scrollTop || document.body.scrollTop
+        
+        scroll >= 200 ?
+        this.setState({
+            alto : 60,
+            background : 'tomato'
+        }) :
+        
+        this.setState({
+            alto : 80,
+            background : 'transparent'
+        })
+    }
+    
     render() {
+        const styles = {
+            height : this.state.alto,
+            background : this.state.background
+        }
+        
         const a = ['home','blog', 'about'],
               list = a.map( i => <a href='#'>{i}</a> )
+              
         return(
-            <div className="navBar">
-                <h2>Logo</h2>
-                <nav className="nav">
-                    {list}
-                </nav>
+            <div className="navBar" style={styles}>
+                <div className="navBar__container">
+                    <h2>Logo</h2>
+                    <nav className="nav">
+                        {list}
+                    </nav>
+                </div>
             </div>
         )
     }
